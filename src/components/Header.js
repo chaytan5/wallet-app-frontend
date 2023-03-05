85f6fa835dfebcc9a4de032f398ec5e8daa0bc5c
Chaytan Arora <chaytanarora@gmail.com>
2023-03-06 01:51:33 +0530

    add link to homepage on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
 	return (
 		<div className="header">
-			<h1 className="app-heading">Wallet App</h1>
+			<a href="/">
+				<h1 className="app-heading">Wallet App</h1>
+			</a>
 		</div>
 	);
 };
