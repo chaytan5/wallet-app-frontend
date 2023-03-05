4630f44bddd40f273cfdc679c5f07e1dd9219ab2
Chaytan Arora <chaytanarora@gmail.com>
2023-03-06 00:18:43 +0530

    add sort functionality of table data

--- src/components/TransactionDetails.js
+++ src/components/TransactionDetails.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Pagination from "./Pagination";
 import TransactionTable from "./TransactionTable";
-
+import localData from "../data.json";
 /**
- * features in this  component:
+ * features in this component:
  * sort by date and amount
  * pagination links
  * export csv file
- *
  */
 
 const TransactionDetails = () => {
-	const [transactions, setTransactions] = useState([]);
+	const [transactions, setTransactions] = useState(localData);
 	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [transactionsPerPage] = useState(15);
+	const [order, setOrder] = useState("ASC");
 
 	useEffect(() => {
-		getTransactions();
+		// getTransactions();
 	}, []);
 
 	const getTransactions = async () => {
@@ -26,7 +26,7 @@ const TransactionDetails = () => {
 		const json = await res.json();
 		setLoading(false);
 
-		setTransactions(json);
+		// setTransactions(json);
 	};
 
 	// get current transactions
@@ -42,11 +42,32 @@ const TransactionDetails = () => {
 		setCurrentPage(pageNumber);
 	};
 
-	console.log(transactions);
+	// sorting
+	const sorting = (col) => {
+		if (order === "ASC") {
+			const sorted = [...transactions].sort((a, b) =>
+				a[col] > b[col] ? 1 : -1
+			);
+			setTransactions(sorted);
+			setOrder("DESC");
+		}
+		if (order === "DESC") {
+			const sorted = [...transactions].sort((a, b) =>
+				a[col] < b[col] ? 1 : -1
+			);
+			setTransactions(sorted);
+			setOrder("ASC");
+		}
+	};
+
 	return (
 		<div className="trx-container">
 			<h2>Recent Transactions</h2>
-			<TransactionTable transactions={currentTransactions} loading={loading} />
+			<TransactionTable
+				transactions={currentTransactions}
+				loading={loading}
+				sorting={sorting}
+			/>
 			<Pagination
 				transactionsPerPage={transactionsPerPage}
 				totalTransactions={transactions.length}
--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,29 +1,38 @@
 import React from "react";
 
-/**
- * table
- *  table row
- *  table data
- */
-const TransactionTable = ({ transactions, loading }) => {
+const TransactionTable = ({ transactions, loading, sorting }) => {
 	if (loading) {
 		return <h3>Loading...</h3>;
 	}
 
 	return (
 		<div className="table-container">
+			<p>Click on Date or Amount heading to sort the data</p>
 			<table className="transaction-table">
-				<tr className="table-header">
-					<th>Date</th>
-					<th>Type</th>
-					<th>Amount</th>
-					<th>Balance</th>
-				</tr>
-				{transactions.map((item) => (
-					<tr className="table-row" key={item.id}>
-						<td className="table-cell">{item.title}</td>
+				<thead>
+					<tr className="table-header">
+						<th className="th-date" onClick={() => sorting("date")}>
+							Date
+						</th>
+						<th>Type</th>
+						<th className="th-amount" onClick={() => sorting("amount")}>
+							Amount
+						</th>
+						<th>Balance</th>
 					</tr>
-				))}
+				</thead>
+				<tbody>
+					{transactions.map((item) => (
+						<tr className="table-row" key={item.id}>
+							<td className="table-cell">
+								{new Date(item?.date)?.toLocaleDateString()}
+							</td>
+							<td className="table-cell">{item.type}</td>
+							<td className="table-cell">{item.amount}</td>
+							<td className="table-cell">{item.balance}</td>
+						</tr>
+					))}
+				</tbody>
 			</table>
 		</div>
 	);
